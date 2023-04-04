a67221b3b36afa4b5a84c840e5c22276cc8d3432
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-04-04 12:36:57 +0000

    Deploying to gh-pages from @ NazarSak/goit-react-hw-05-movies@bc91c6c6414616492bd4dc22a436b9f6d6f44e72 🚀

---
+++ static/js/940.7bd712f3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[940],{940:function(e,r,t){t.r(r);var n=t(184);r.default=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("h2",{children:"Sorry page not found"})})}}}]);
+//# sourceMappingURL=940.7bd712f3.chunk.js.map
